0ece84bdc1bbf9d33fa9d07534e8bcf6efd7b4e1
Václav Jančařík <vaclav.janc@gmail.com>
2018-03-31 17:19:40 +0200

    Add showMore button to StoredBracketList

--- src/views/StoredBracketList.view.vue
+++ src/views/StoredBracketList.view.vue
@@ -1,12 +1,15 @@
 <template>
-  <ul>
-    <li v-for="bracket of brackets" :key="bracket.id">
-      <!-- The indentation is shitty because HTML is shitty. -->
-      <router-link :to="{ name: 'bracket', params: { id: bracket.id } }">
-        {{ bracket.name || 'Unnamed bracket' }}</router-link>,
-        last modified {{ distanceInWordsToNow(bracket.lastModified)}} ago
-    </li>
-  </ul>
+    <div>
+    <ul>
+      <li v-for="(bracket, index) of brackets" :key="bracket.id" v-if="index < limit">
+        <!-- The indentation is shitty because HTML is shitty. -->
+        <router-link :to="{ name: 'bracket', params: { id: bracket.id } }">
+          {{ bracket.name || 'Unnamed bracket' }}</router-link>,
+          last modified {{ distanceInWordsToNow(bracket.lastModified)}} ago
+      </li>
+    </ul>
+    <button @click="showMore()" type="button" v-if="brackets.length > limit">Show more</button>
+  </div>
 </template>
 
 <script>
@@ -15,6 +18,8 @@ import { Component } from "vue-property-decorator"
 import localforage from "localforage"
 import * as R from "ramda"
 import { distanceInWordsToNow } from "date-fns"
+
+const DEFAULT_BRACKET_LIMIT = 10
 
 @Component({
   created() {
@@ -29,6 +34,11 @@ import { distanceInWordsToNow } from "date-fns"
 export default class StoredBracketList extends Vue {
   brackets = []
   distanceInWordsToNow = distanceInWordsToNow
+  limit = DEFAULT_BRACKET_LIMIT
+
+  showMore() {
+    this.limit += DEFAULT_BRACKET_LIMIT
+  }
 }
 </script>
 
