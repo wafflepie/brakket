9530b52e9bc609eb4228b61e4f84ba3214dc0805
Václav Jančařík <vaclav.janc@gmail.com>
2018-04-28 22:07:40 +0200

    Serve index.html on each path

--- src/server/index.js
+++ src/server/index.js
@@ -5,17 +5,25 @@ const serve = require("koa-static")
 const http = require("http")
 const SocketIO = require("socket.io")
 const mongoose = require("mongoose")
+const { createReadStream } = require("fs")
+
 const attachRoutes = require("./routes")
 
+const DIST_PATH = path.join(__dirname, "../../dist")
 const app = new Koa()
 const server = http.createServer(app.callback())
 const io = SocketIO(server)
 
-app.use(serve(path.join(__dirname, "../../dist")))
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test")
 
 app.context.io = io
+
+app.use(serve(DIST_PATH))
+
+app.use(async ctx => {
+  ctx.type = "html"
+  ctx.body = createReadStream(path.join(DIST_PATH, "index.html"))
+})
 
 attachRoutes(io)
 
